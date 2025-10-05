220a8d4e81909c92f8cabc34667afbdf27c54767
Swanky <30837558+YUND4@users.noreply.github.com>
2025-10-05 07:54:06 -0500

    more changes

---
+++ alembic/versions/a8a5b7e07f81_update_annotations_schema.py
@@ -0,0 +1,27 @@
+"""
+
+Revision ID: 
+Revises: 
+Create Date: 
+
+"""
+from __future__ import annotations
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = 
+down_revision = 
+branch_labels = 
+depends_on = 
+
+
+def upgrade() -> None:
+    """Apply database changes."""
+    pass
+
+
+def downgrade() -> None:
+    """Rollback database changes."""
+    pass
